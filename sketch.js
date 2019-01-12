f71112d00192dda17b9126667e7171b910fd891d
Ben Yip <bmyip@ncsu.edu>
2019-01-12 16:18:11 -0500

    closes #1 (for now)

--- sketch.js
+++ sketch.js
@@ -1,45 +1,51 @@
+var smallScreen = $(window).width() < 800;
+
+// reload window when increasing window size above threshold
+$(window).resize(function() {
+    var cw = $(window).width();
+    if (cw < 800) {
+        smallScreen = true;
+    }
+    if (cw > 800 & smallScreen) {
+        location.reload();
+    }
+})
+
 if ($(window).width() > 800) {    
     var e = document.getElementById('target');
     var e_loc = e.getBoundingClientRect();
-    console.log(e_loc)
 
     var s = document.querySelector('.current');
     var s_loc = s.getBoundingClientRect();
-
-    var v_dist = e_loc.top - s_loc.bottom;
-    var h_dist = s_loc.right - e_loc.x + 100
-
 
     function setup() {
         canvas = createCanvas(windowWidth, windowHeight);
         canvas.position(0, 0);
         canvas.style('z-index', '-1');
-        test = new DotConnector(s_loc, e_loc);
+        current = new DotConnector(s_loc, e_loc);
     }
-
 
     function windowResized() {
        if ($(window).width() > 800) {
          e_loc = e.getBoundingClientRect();
          s_loc = s.getBoundingClientRect();
          resizeCanvas(windowWidth, windowHeight);
-         test = new DotConnector(s_loc, e_loc);
+         current = new DotConnector(s_loc, e_loc);
        } else {
           remove();
        }
     }
 
     function draw() {
-        test.display();
-        test.next();
+        current.display();
+        current.next();
     }
 
     function DotConnector(s, e) {
-        this.x_start = s.right;
-        this.y_start = s.top;
+
         this.x_end = e.x + e.width / 2;
         this.y_end = e.top;
-        this.x = this.x_start;
+        this.x = s.right;
         this.y = s.bottom + 2;
 
         this.display = function() {
@@ -48,17 +54,15 @@ if ($(window).width() > 800) {
         }
 
         this.next = function() {
-                if (this.x >= s.left) {
-                    line(s.right, s.bottom + 2, s.left, s.bottom + 2);
-                    this.x = s.left - 1;
-                } else if (this.x >= this.x_end) {
-                    this.x += -6;
-                } else if (this.y <= this.y_end) {
-                    this.y += 6;
-                }
+            if (this.x >= s.left) {
+                line(s.right, s.bottom + 2, s.left, s.bottom + 2);
+                this.x = s.left - 1;
+            } else if (this.x >= this.x_end) {
+                this.x += -6;
+            } else if (this.y <= this.y_end) {
+                this.y += 6;
+            }
         }
     }  
-} else {
-    // do nothing
-}
-  
+} 
+
